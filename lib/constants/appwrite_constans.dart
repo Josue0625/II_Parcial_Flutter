20eca4f84c1851acecbca4f9f01c44901a75eeb5
Josue0625 <jcampos@ufpso.edu.co>
2023-05-16 01:53:42 -0500

    Se creo el login y el signup y se conectaron las dos interfaces

---
+++ lib/constants/appwrite_constans.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '646301ee199e8ca10ddf';
+  static const String projectId = '6462fb0a0e5130fb884b';
+  static const String endPoint = 'http://localhost:80/v1';
+}
