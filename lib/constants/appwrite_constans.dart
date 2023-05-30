3737f886f294cd79d8e7492bdbe4c3ef8e0b4c10
Josue0625 <jcampos@ufpso.edu.co>
2023-05-29 20:39:06 -0500

    se registran los usuarios y ademas se valida el login, tambien si un usuario no existe lo manda a la pagina de signup, se empezo a hacer todo lo concerniente al modelo de usuario en el que se toman todos los datos del mismo

--- lib/constants/appwrite_constans.dart
+++ lib/constants/appwrite_constans.dart
@@ -1,5 +1,14 @@
 class AppwriteConstants {
   static const String databaseId = '646301ee199e8ca10ddf';
   static const String projectId = '6462fb0a0e5130fb884b';
-  static const String endPoint = 'http://localhost:80/v1';
+  static const String endPoint = 'http://192.168.101.7:80/v1';
+
+  static const String usersCollection = '6475063adb782f7edd52';
+  static const String tweetsCollection = '63cbd6781a8ce89dcb95';
+  static const String notificationsCollection = '63cd5ff88b08e40a11bc';
+
+  static const String imagesBucket = '63cbdab48cdbccb6b34e';
+
+  static String imageUrl(String imageId) =>
+      '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
 }
--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
-class SignUpView extends StatefulWidget {
+import '../../../common/loading_page.dart';
+
+class SignUpView extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
         builder: (context) => const SignUpView(),
       );
   const SignUpView({super.key});
 
   @override
-  State<SignUpView> createState() => _SignUpViewState();
+  ConsumerState<SignUpView> createState() => _SignUpViewState();
 }
 
-class _SignUpViewState extends State<SignUpView> {
+class _SignUpViewState extends ConsumerState<SignUpView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -28,11 +32,19 @@ class _SignUpViewState extends State<SignUpView> {
     passwordController.dispose();
   }
 
+  void onSignUp() {
+    ref.read(authControllerProvider.notifier).signUp(
+        email: emailController.text,
+        password: passwordController.text,
+        context: context);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: appbar,
-      body: Center(
+      body: isLoading ? const Loader() : Center(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -56,7 +68,7 @@ class _SignUpViewState extends State<SignUpView> {
                 Align(
                   alignment: Alignment.topRight,
                   child: RoundedSmallButton(
-                    onTap: () {},
+                    onTap: onSignUp,
                     label: 'Done',
                   ),
                 ),
